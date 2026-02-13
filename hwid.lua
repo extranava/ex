58dbef82313bff498ffaae6b7b711ddf119e8248
ex <gromantomriw@gmail.com>
2026-02-13 07:09:55 +0700

    Create hwid.lua

---
+++ hwid.lua
@@ -0,0 +1,12 @@
+return {
+    ["KEY-1234-5678"] = {
+        ["HWID"] = "", -- ทิ้งไว้ว่างๆ เพื่อให้คนแรกที่รันเป็นเจ้าของ
+        ["Expires"] = 20261231, -- วันหมดอายุ (YYYYMMDD)
+        ["Status"] = "Available"
+    },
+    ["KEY-PREMIUM-99"] = {
+        ["HWID"] = "",
+        ["Expires"] = 20260520,
+        ["Status"] = "Available"
+    }
+}
